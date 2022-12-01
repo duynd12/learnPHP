0005b05a53eb034e5e76a91ba66d4939f72a1c91
duynd12 <duy88706@gmail.com>
2022-12-01 22:44:00 +0700

    change

--- index.php
+++ index.php
@@ -15,76 +15,93 @@ include './submit.php';
 </head>
 
 <body>
-    <form action="" method="post">
-        <div class=wrapper>
-            <div class="container">
-                <div class="box_username">
-                    <label class="title">Tên : <span class="star">*</span></label>
-                    <br>
-                    <input type=" text" name="username" id="username">
-                    <br>
-                    <span class="text_error"><?php echo $error_username; ?></span>
-                    <br>
-                </div>
-                <div>
-                    <label class="title">Tuổi : <span class="star">*</span></label>
-                    <br>
-                    <input type="text" name="age" id="age">
-                    <br>
-                    <span class="text_error"><?php echo $error_age; ?></span>
-                    <br>
+    <?php if ($isDisplay) { ?>
+        <form action="" method="post">
+            <div class=wrapper>
+                <div class="container">
+                    <div class="box_username">
+                        <label class="title">Tên : <span class="star">*</span></label>
+                        <br>
+                        <input type=" text" name="username" id="username">
+                        <br>
+                        <div class="text_error"><?php echo $error_username; ?></div>
+                        <br>
+                    </div>
+                    <div style="margin-top:-20px">
+                        <label class="title">Tuổi :</label>
+                        <br>
+                        <input type="text" name="age" id="age">
+                        <br>
+                    </div>
+                    <div>
+                        <label class="title">Địa chỉ :</label>
+                        <br>
+                        <input type="text" name="address" id="address">
+                        <br>
+                    </div>
+                    <div>
+                        <label class="title">Số điện thoại : <span class="star">*</span></label>
+                        <br>
+                        <input type="text" name="numberphone">
+                        <br>
+                        <span class="text_error"><?php echo $error_numberphone; ?></span>
 
-                </div>
-                <div>
-                    <label class="title">Địa chỉ : <span class="star">*</span></label>
-                    <br>
-                    <input type="text" name="address" id="address">
-                    <br>
-                    <span class="text_error"><?php echo $error_address; ?></span>
+                        <br>
+                    </div>
+                    <div style="margin-top:-20px">
+                        <label class="title">Nghề nghiệp :</label>
+                        <br>
+                        <input type="text" name="job">
+                        <br>
+                    </div>
+                    <div>
+                        <label class="title">Email : <span class="star">*</span></label>
+                        <br>
+                        <input type="text" name="email">
+                        <br>
+                        <span class="text_error"><?php echo $error_email; ?></span>
 
-                    <br>
+                        <br>
+                    </div>
+                    <div style="margin-top:-20px">
+                        <label class="title">Password : <span class="star">*</span></label>
+                        <br>
+                        <input type="text" name="password">
+                        <br>
+                        <span class="text_error"><?php echo $error_password; ?></span>
 
+                        <br>
+                    </div>
+                    <input type="submit"></input>
                 </div>
-                <div>
-                    <label class="title">Số điện thoại : <span class="star">*</span></label>
-                    <br>
-                    <input type="text" name="numberphone">
-                    <br>
-                    <span class="text_error"><?php echo $error_numberphone; ?></span>
-
-                    <br>
-                </div>
-                <div>
-                    <label class="title">Nghề nghiệp : <span class="star">*</span></label>
-                    <br>
-                    <input type="text" name="job">
-                    <br>
-                    <span class="text_error"><?php echo $error_job; ?></span>
-
-                    <br>
-                </div>
-                <div>
-                    <label class="title">Email : <span class="star">*</span></label>
-                    <br>
-                    <input type="text" name="email">
-                    <br>
-                    <span class="text_error"><?php echo $error_email; ?></span>
-
-                    <br>
-                </div>
-                <div>
-                    <label class="title">Password : <span class="star">*</span></label>
-                    <br>
-                    <input type="text" name="password">
-                    <br>
-                    <span class="text_error"><?php echo $error_password; ?></span>
-
-                    <br>
-                </div>
-                <input type="submit"></input>
+            </div>
+        </form>
+    <?php } else { ?>
+        <div class="info">
+            <h1>Thông tin người dùng</h1>
+            <div class="info_name">
+                Tên : <h3><?php echo $username; ?></h3>
+            </div>
+            <div class="info_name">
+                Tuổi : <h3><?php echo $age; ?></h3>
+            </div>
+            <div class="info_name">
+                Địa chỉ : <h3><?php echo $address; ?></h3>
+            </div>
+            <div class="info_name">
+                Số Điện Thoại : <h3><?php echo $numberphone; ?></h3>
+            </div>
+            <div class="info_name">
+                Nghề Nghiệp : <h3><?php echo $job; ?></h3>
+            </div>
+            <div class="info_name">
+                Email : <h3><?php echo $email; ?></h3>
+            </div>
+            <div class="info_name">
+                Mật khẩu : <h3><?php echo $password; ?></h3>
             </div>
         </div>
-    </form>
+    <?php } ?>
 </body>
 
 </html>
--- submit.php
+++ submit.php
@@ -3,6 +3,8 @@ include './regexName.php';
 include './const.php';
 $s = "";
 $array = array();
+$isDisplay = True;
+$convertString = '';
 if (isset($_POST['username']) || isset($_POST['age']) || isset($_POST['address']) || isset($_POST['numberphone']) || isset($_POST['job']) || isset($_POST['email']) || isset($_POST['password'])) {
 
     $username = $_POST['username'];
@@ -16,21 +18,13 @@ if (isset($_POST['username']) || isset($_POST['age']) || isset($_POST['address']
     if ($username == "") {
         $error_username = "Vui lòng không được để trống";
     }
-    if ($age == "") {
-        $error_age = "Vui lòng không được để trống";
-    } else if (is_numeric($age) || (0 >= (int)$age || (int)$age >= 101)) {
-        $error_age = "Vui lòng nhập số tuổi hợp lệ";
-    }
-    if ($address == "") {
-        $error_address = "Vui lòng không được để trống";
-    }
+    // if ((0 >= (int)$age || (int)$age >= 101)) {
+    //     $error_age = "Vui lòng nhập số tuổi hợp lệ";
+    // }
     if ($numberphone == "") {
         $error_numberphone = "Vui lòng không được để trống";
     } else if (!check_regexEmail($regexPhoneNumber, $numberphone)) {
         $error_numberphone = "Số điện thoại không đúng định dạng";
-    }
-    if ($job == "") {
-        $error_job = "Vui lòng không được để trống";
     }
     if ($email == "") {
         $error_email = "Vui lòng không được để trống";
@@ -43,15 +37,14 @@ if (isset($_POST['username']) || isset($_POST['age']) || isset($_POST['address']
         $error_password = "Mật khẩu không hợp lệ";
     }
 
-    echo "Tên : " . $username . "<br/>";
-    echo "Tuổi : " . $age . "<br/>";
-    echo "Địa chỉ : " . $address . "<br/>";
-    echo "Số điện thoại : " . $numberphone . "<br/>";
-    echo "Nghề Nghiệp : " . $job . "<br/>";
-    echo "Email : " . $email . "<br/>";
-
     for ($i = 0; $i < strlen($password); $i++) {
         $array[$i] = '* ';
     }
-    echo "Mật khẩu : " . implode(" ", $array);
+    $convertString = implode(" ", $array);
+    if (
+        !empty($password) && check_regexEmail($regexPassword, $password) && !empty($numberphone) && !empty($email) && !empty($username) && check_regexEmail($regexEmail, $email)
+        && check_regexEmail($regexPhoneNumber, $numberphone)
+    ) {
+        $isDisplay = false;
+    }
 }
